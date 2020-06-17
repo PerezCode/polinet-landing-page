4c950f5a26df94d995f15696ae3adef283297d89
DanielRamirez1023 <danielfelipe23.dr@gmail.com>
2020-06-16 20:11:47 -0500

    campo de agregar imagen terminado

--- src/js/registroObjetos.js
+++ src/js/registroObjetos.js
@@ -1,8 +1,41 @@
 $(function(){
 
-    $("#circuloImagen").click(function() {
-        $("input[type=file]").click();
-    });
+const imagePreview = document.getElementById('img-preview');
+const barrita = document.getElementById('img-upload-bar');
+
+const CLOUDINARY_URL = "https://api.cloudinary.com/v1_1/servidor/image/upload";
+const CLOUDINARY_UPLOAD_PRESET = "zllegocx";
+
+$("#img-uploader").on('change', async (e) => {
+
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESET);
+
+    const res = await axios.post(
+        CLOUDINARY_URL,
+        formData,
+        {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            },
+            onUploadProgress (e) {
+                let progress = Math.round((e.loaded * 100.0) / e.total);
+                console.log();
+                barrita.setAttribute('value', progress);
+            }
+        }
+    );
+    imagePreview.src = res.data.secure_url;
+
+    $("#img-uploader").addClass("quitarInput");
+    $("#img-upload-bar").addClass("quitarInput");
+
+});
+
+var shieldHomeContainer = nodeCollection.SelectSingleNode("//img").Attributes["src"].Value;
+console.log(shieldHomeContainer)
 
     $("#boton2").click(function(){
         let informacion = [];
